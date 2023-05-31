54d9e08db561619e7f029cec3003c43c5cb0fc70
i-ga-ra-shi <kigararhi@gmail.com>
2023-05-31 20:17:22 +0900

    newアクションとcreateアクション

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,10 +4,21 @@ class SubjectsController < ApplicationController
     end
 
     def new
-
+        @subject = Subject.new
     end
 
     def create
-        
+        @subject = Subject.new(subject_params)
+        if @subject.save
+            redirect_to(root_path)
+        else
+            render(:new)
+        end
     end
+
+    private
+    def subject_params
+        params.require(:subject).permit(:subject, :title, :name, :content).merge(user_id: current_user.id)
+    end
+
 end

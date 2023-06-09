336ca419822753f8666c25b745f87c00bdc4ef95
i-ga-ra-shi <kigararhi@gmail.com>
2023-06-09 16:44:00 +0900

    詳細ページ(2)

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -2,6 +2,32 @@ class PublicsController < ApplicationController
 
     def index
         @subjects = Subject.where(id: Public.pluck(:subject_id))
+        if params[:subject_name]
+            @subjects = Subject.subject_name(params[:subject_name])
+            if params[:latest]
+                @subjects = @subjects.latest
+            elsif params[:old]
+                @subjects = @subjects.old
+            elsif params[:subject_id]
+                @subjects = @subjects.subject_id
+            elsif params[:month_id]
+                @subjects = @subjects.month_id
+            elsif params[:student_id]
+                @subjects = @subjects.student_id
+            end
+        elsif params[:latest]
+            @subjects = Subject.latest
+        elsif params[:old]
+            @subjects = Subject.old
+        elsif params[:subject_id]
+            @subjects = Subject.subject_id
+        elsif params[:month_id]
+            @subjects = Subject.month_id
+        elsif params[:student_id]
+            @subjects = Subject.student_id
+        else
+            @subjects = Subject.includes(:user)
+        end
     end
 
     def create
--- app/models/public.rb
+++ app/models/public.rb
@@ -1,6 +1,19 @@
 class Public < ApplicationRecord
-
     belongs_to :user
     belongs_to :subject
 
+    scope :latest, -> {order(created_at: :desc)}
+    scope :old, -> {order(created_at: :asc)}
+    scope :subject_id, -> {order(subject_id: :asc)}
+    scope :month_id, -> {order(month_id: :asc)}
+    scope :student_id, -> {order(student_id: :asc)}
+    scope :subject_name, ->(subject_id) { where("subject_id = ?", subject_id) }
+
+    def self.search(keyword)
+        if keyword.present?
+            Subject.where('sub_subject LIKE ? OR title LIKE ? OR name LIKE ? OR content LIKE ?', "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
+        else
+            Subject.includes(:user)
+        end
+    end
 end
